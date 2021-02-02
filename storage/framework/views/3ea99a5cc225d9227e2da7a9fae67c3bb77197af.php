35ffba6cbf5cc4a5be6fabbc7bac88af51974a80
9090ID <farhan@unja.ac.id>
2021-02-02 11:15:58 +0700

    multibahasa laravel

---
+++ storage/framework/views/3ea99a5cc225d9227e2da7a9fae67c3bb77197af.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+ 
+<head>
+    <title><?php echo e(config('app.name')); ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+ 
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container">
+        <a class="navbar-brand" href="#"><?php echo e(config('app.name')); ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+ 
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo e(__('home.switch_language')); ?>
+
+                    </a>
+                  
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item <?php echo e(app()->getLocale() == 'en' ? 'active' : ''); ?>" href="<?php echo e(route('localization.switch', 'en')); ?>">English</a>
+                        <a class="dropdown-item <?php echo e(app()->getLocale() == 'id' ? 'active' : ''); ?>" href="<?php echo e(route('localization.switch', 'id')); ?>">Bahasa Indonesia</a>
+                  
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container pt-5">
+        <h1><?php echo e(__('home.title')); ?></h1>
+        <p><?php echo e(__('home.description')); ?></p> 
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ 
+</html>
+ <?php /**PATH C:\xampp\htdocs\multibahasaasli\resources\views/welcome.blade.php ENDPATH**/ ?>
